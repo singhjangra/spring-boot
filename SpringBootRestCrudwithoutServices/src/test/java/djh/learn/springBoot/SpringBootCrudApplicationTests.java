fc68b610a18b5b0cef804e8fabd53345f2464a1f
Sunil Jangra <Sunil Jangra@Sunil>
2021-12-06 19:35:51 +0530

    spring boot crud rest without services using jpo's

---
+++ SpringBootRestCrudwithoutServices/src/test/java/djh/learn/springBoot/SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package djh.learn.springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
